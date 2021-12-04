ada0b24428eda1e60ae89d7875737fd80fff95ce
Balasubramaniyan Raguraman <balasubramaniyan.raguraman@kanini.com>
2021-12-04 12:01:34 +0530

    New

--- app/src/main/java/com/example/task3/MainActivity.kt
+++ app/src/main/java/com/example/task3/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.task3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
@@ -12,20 +13,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var number1= findViewById<EditText>(R.id.guessNumber).text
         var number2 =findViewById<TextInputLayout>(R.id.guessingNumber).editText?.text
+        var count=0
         findViewById<TextView>(R.id.answer).setOnClickListener()
         {
-            if (number1.toString().toInt() > number2.toString().toInt()) {
-                findViewById<TextView>(R.id.hint).setText("No :) My number is Bigger")
-            } else if(number1.toString().toInt() < number2.toString().toInt()) {
-                findViewById<TextView>(R.id.hint).setText("No :) My number is Smaller")
+            count++
+            val newScreenIntent=Intent(this,MessageActivity::class.java)
+            if (number1.toString().toInt() == number2.toString().toInt() && count<=12) {
+                newScreenIntent.putExtra("value","Yes")
+                newScreenIntent.putExtra("answer",number1.toString())
+                startActivity(newScreenIntent)
+                count=0
             }
-            else if(number1.toString().toInt()==number2.toString().toInt())
+            else if(number1.toString().toInt() != number2.toString().toInt()&&count==12)
             {
-                findViewById<TextView>(R.id.hint).setText("You are right")
+                newScreenIntent.putExtra("value","No")
+                newScreenIntent.putExtra("answer",number1.toString())
+                startActivity(newScreenIntent)
+                count=0
             }
             else
             {
-                findViewById<TextView>(R.id.hint).setText("Enter the Values")
+                findViewById<TextView>(R.id.hint).setText("Try Answer is wrong")
             }
         }
     }
